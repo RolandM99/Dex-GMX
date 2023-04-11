85dd6279d1f1e814d4fd38c785d039ba317225ba
rolandm99 <manfulmweze99@gmail.com>
2023-04-11 15:25:07 +0200

    Add the mapping method through tokens

--- src/telegram/tg_bot.ts
+++ src/telegram/tg_bot.ts
@@ -10,9 +10,35 @@ interface UserState {
   amount?: number;
 }
 
+
+const Tokens = [
+  {
+      "address": "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+      "symbol": "ETH",
+      "name": "Ethereum"
+  },
+  {
+      "address": "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f",
+      "symbol": "WBTC",
+      "name": "Bitcoin"
+  },
+  {
+      "address": "0xf97f4df75117a78c1A5a0DBb814Af92458539FB4",
+      "symbol": "LINK",
+      "name": "Chainlink"
+  },
+  {
+      "address": "0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0",
+      "symbol": "UNI",
+      "name": "Uniswap"
+  }
+]
+
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
 
-const tokens = ['USDC/ETH', 'USDC/BTC', 'USDC/LINK', 'USDC/UNI'];
+const tokens = Tokens.map((token) => `USDC / ${token.symbol}`);
+
+
 const leverages = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
 const numRows = 2;
 const numCols = 2;
