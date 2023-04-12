1c8c069ff8de9596ccac839b7d5ea3469f448740
AnicetFantomas <anicetmurhula20@gmail.com>
2023-04-12 14:02:47 +0200

    Get current price for the selected token and shot or long boolean

--- src/telegram/tg_bot.ts
+++ src/telegram/tg_bot.ts
@@ -1,24 +1,18 @@
-const CallbackQuery  = require('node-telegram-bot-api')
-const { Telegraf, Markup } = require('telegraf')
-import Tokens from './data';
-const axios = require('axios');
+const CallbackQuery = require("node-telegram-bot-api");
+const { Telegraf, Markup } = require("telegraf");
+import Tokens from "./data";
+const axios = require("axios");
 
-require ('dotenv').config()
+require("dotenv").config();
 
 interface UserState {
   token?: string;
-  longShort?: 'long' | 'short';
+  longShort?: boolean;
   leverage?: number;
   amount?: number;
 }
 
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
-
-const handleSelectedSymbol = (symbol: string) => {
-  const selectedToken: any = Tokens.find(token => token.symbol === symbol)
-  return selectedToken ? selectedToken.address : ""
-}
-
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 const tokens = Tokens.map((token) => `USDC/${token.symbol}`);
 
@@ -28,108 +22,137 @@ const numCols = 2;
 
 const state: Record<number, UserState> = {};
 
-const tokenMenu = Telegraf.Extra
-  .markdown()
-  .markup((m:any) => {
-    const keyboard = [];
+const tokenMenu = Telegraf.Extra.markdown().markup((m: any) => {
+  const keyboard = [];
 
-    for (let i = 0; i < tokens.length; i += numCols) {
-      const row = [];
+  for (let i = 0; i < tokens.length; i += numCols) {
+    const row = [];
 
-      for (let j = 0; j < numCols; j++) {
-        const tokenIndex = i + j;
-        if (tokenIndex >= tokens.length) {
-          break;
-        }
-
-        const token = tokens[tokenIndex];
-        row.push(m.callbackButton(token, `select_token_${token}`));
+    for (let j = 0; j < numCols; j++) {
+      const tokenIndex = i + j;
+      if (tokenIndex >= tokens.length) {
+        break;
       }
 
-      keyboard.push(row);
+      const token = tokens[tokenIndex];
+      row.push(m.callbackButton(token, `select_token_${token}`));
     }
 
-    return m.inlineKeyboard(keyboard);
-  });
+    keyboard.push(row);
+  }
 
-
-  const longShortMenu = Telegraf.Extra
-  .markdown()
-  .markup((m:any) => m.inlineKeyboard([
-    m.callbackButton('Long', 'select_long'),
-    m.callbackButton('Short', 'select_short'),
-  ]));
-
-const leverageMenu = Telegraf.Extra
-  .markdown()
-  .markup((m:any) => m.inlineKeyboard(
-    leverages.map((leverage) => m.callbackButton(`${leverage}x`, `select_leverage_${leverage}`))
-  ));
-
-bot.start((ctx:any) => {
-    console.log("New user has joined the bot")
-    state[ctx.from!.id] = {};
-ctx.reply('🚀Welcome to NgGmxBot!🚀 \n A bot that facilitate trading on the GMX Dex platform on easy to use way  \n\n Please select a token:', tokenMenu )
-})
-
-bot.action(/^select_token_(.*)$/, (ctx:any) => {
-  const token = ctx.match[1];
-  state[ctx.from!.id].token = token;
-  ctx.reply(`You selected ${token}. Long or short?`, longShortMenu);
+  return m.inlineKeyboard(keyboard);
 });
 
-bot.action('select_long', (ctx:any) => {
-  state[ctx.from!.id].longShort = 'long';
-  ctx.reply('Please select leverage:', leverageMenu);
+const longShortMenu = Telegraf.Extra.markdown().markup((m: any) =>
+  m.inlineKeyboard([
+    m.callbackButton("Long", "select_long"),
+    m.callbackButton("Short", "select_short"),
+  ])
+);
+
+const leverageMenu = Telegraf.Extra.markdown().markup((m: any) =>
+  m.inlineKeyboard(
+    leverages.map((leverage) =>
+      m.callbackButton(`${leverage}x`, `select_leverage_${leverage}`)
+    )
+  )
+);
+
+bot.start((ctx: any) => {
+  console.log("New user has joined the bot");
+  state[ctx.from!.id] = {};
+  ctx.reply(
+    "🚀Welcome to NgGmxBot!🚀 \n A bot that facilitate trading on the GMX Dex platform on easy to use way  \n\n Please select a token:",
+    tokenMenu
+  );
 });
 
-bot.action('select_short', (ctx:any) => {
-  state[ctx.from!.id].longShort = 'short';
-  ctx.reply('Please select leverage:', leverageMenu);
+bot.action(/^select_token_(.*)$/, async (ctx: any) => {
+  const symbol = ctx.match[1].split("/")[1];
+  const token = Tokens.find((t) => t.symbol === symbol);
+
+  console.log({ symbol });
+
+  if (token) {
+    const address = token.address;
+
+    const response = await fetch("https://api.gmx.io/prices");
+    const prices = await response.json();
+
+    const tokenPrice = prices[address];
+
+    console.log(tokenPrice);
+    ctx.reply(`You selected ${symbol} and its current price on the market is: ${tokenPrice}`);
+
+  } else {
+    ctx.reply("Token not found");
+  }
+
+  ctx.reply(` Long or short?`, longShortMenu);
 });
 
-bot.action(/^select_leverage_(.*)$/, (ctx:any) => {
+bot.action("select_long", (ctx: any) => {
+  state[ctx.from!.id].longShort = true;
+  ctx.reply("You selected long");
+  ctx.reply("Please select leverage:", leverageMenu);
+});
+
+bot.action("select_short", (ctx: any) => {
+  state[ctx.from!.id].longShort = false;
+  ctx.reply("You selected short");
+  ctx.reply("Please select leverage:", leverageMenu);
+});
+
+bot.action(/^select_leverage_(.*)$/, (ctx: any) => {
   const leverage = parseInt(ctx.match[1], 10);
   state[ctx.from!.id].leverage = leverage;
-  ctx.reply('Please enter the order amount:');
+  ctx.reply("Please enter the order amount:");
 });
 
-bot.on('text', (ctx:any) => {
+bot.on("text", (ctx: any) => {
   const amount = parseFloat(ctx.message.text);
   if (!isNaN(amount)) {
     state[ctx.from!.id].amount = amount;
-    placeOrder(ctx, `Order placed for ${state[ctx.from!.id].longShort} ${state[ctx.from!.id].leverage}x ${amount} ${state[ctx.from!.id].token}.`);
+    placeOrder(
+      ctx,
+      `Order placed for ${state[ctx.from!.id].longShort} ${
+        state[ctx.from!.id].leverage
+      }x ${amount} ${state[ctx.from!.id].token}.`
+    );
   }
 });
 
-bot.action('cancel_order', (ctx:any) => {
+bot.action("cancel_order", (ctx: any) => {
   state[ctx.from!.id] = {};
-  ctx.reply('Order cancelled.');
+  ctx.reply("Order cancelled.");
 });
 
 bot.launch();
 
 export function placeOrder(ctx: any, message: any) {
   const chatIDs = ["1502424561"];
-  chatIDs.forEach(chat => {
-    bot.telegram.sendMessage(chat, message, {
-      parse_mode: "HTML",
-      disable_web_page_preview: true,
-    }).catch((error: any) => {
-      console.log("Encouterd an error while sending notification to ", chat)
-      console.log(error)
-    })
+  chatIDs.forEach((chat) => {
+    bot.telegram
+      .sendMessage(chat, message, {
+        parse_mode: "HTML",
+        disable_web_page_preview: true,
+      })
+      .catch((error: any) => {
+        console.log("Encouterd an error while sending notification to ", chat);
+        console.log(error);
+      });
   });
-  const { token, longShort, leverage, amount } = state[ctx?.from?.id ?? ''] || {};
+  const { token, longShort, leverage, amount } =
+    state[ctx?.from?.id ?? ""] || {};
   if (!ctx || !ctx.from || !token || !longShort || !leverage || !amount) {
     return;
-  }
- 
-  else {
-    ctx.reply(`Placing order ${longShort} ${leverage}x ${amount} ${token}...`);
+  } else {
+    const orderAmount = amount * leverage; // multiply amount by leverage
+    ctx.reply(`Placing order ${longShort} ${leverage}x ${orderAmount} ${token}...`);
   }
   // TODO: implement the place order function here
-  console.log(`Placing order for ${longShort} ${leverage}x ${amount} ${token}...`);
+  console.log(
+    `Placing order for ${longShort} ${leverage}x ${amount} ${token}...`
+  );
 }
-
-
