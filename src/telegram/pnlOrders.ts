4588d26f83934de6009a07388c2dae33710f53d5
rolandm99 <manfulmweze99@gmail.com>
2023-05-19 19:31:23 +0200

    Update the PNL functionality

--- src/telegram/pnlOrders.ts
+++ src/telegram/pnlOrders.ts
@@ -1,49 +1,21 @@
-import Web3 from "web3";
-import { Order } from "../models/schema";
-import Tokens from "./data";
-import { config } from "../config/config";
-import { utils } from "ethers";
+import axios  from 'axios';
 
+ const getPNL = async (ctx:any) => {
+  try {
+    const apiUrl = 'https://api.gmx.io'; // GMX API URL
+    const endpoint = `/pnl/0x23055E68DAfC3670b20651BD0B2E0Bcd46977b22`; // PNL endpoint for the specific address
+    const response = await axios.get(apiUrl + endpoint);
+    const pnl = response.data;
 
-const web3 = new Web3(config.JSON_RPC);
-
-async function getPrice(tokenAddress: string): Promise<number> {
-  const tokenPrice = await web3.eth.getBalance(tokenAddress);
-  const tokenPriceInEth = Number(web3.utils.fromWei(tokenPrice, "ether"));
-  return tokenPriceInEth;
+    console.log('PNL:', pnl);
+    return pnl;
+  } catch (error) {
+    console.error('Error fetching PNL:', error);
+    throw error;
+  }
 }
 
-function formatCurrency(amount: number, currencyCode: string = "USD"): string {
-  const formatter = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: currencyCode,
-  });
-  return formatter.format(amount);
-}
+export default getPNL;
 
-const getPnl = async (ctx: any) => {
-    // Retrieve all orders placed by the user from the database
-    const orders = await Order.find({}).sort({ createdAt: -1 });
-  
-    if (orders.length === 0) {
-      return ctx.reply("No active orders found.");
-    }
-  
-    // Calculate the total profits and losses for all orders
-    let totalPnl = 0;
-    for (const order of orders) {
-      const token = Tokens.find((t: { address: string; }) => t.address === order.indexToken);
-      const tokenPrice = await getPrice(token?.address ?? "");
-      const usdValue = parseFloat(utils.formatUnits(order.amountIn, 6)) * parseFloat(tokenPrice?.toString() ?? "");
-      const delta = parseFloat(utils.formatUnits(order.sizeDelta, 6));
-      const pnl = order.isLong ? delta : -delta;
-      totalPnl += pnl * usdValue;
-    }
-  
-    // Format the total P&L as USD
-    const formattedPnl = formatCurrency(totalPnl);
-  
-    ctx.reply(`Total P&L: ${formattedPnl}`);
-  };
 
-  export default getPnl;
+
